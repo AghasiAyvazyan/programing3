b3498938c96c8b8b91843e8d00f82398bf2fee0c
AghasiAyvazyan <aghasi.ayvazyan@tumo.org>
2023-04-15 12:50:46 +0400

    start server

--- GameOfLife/server.js
+++ GameOfLife/server.js
@@ -1,1 +1,7 @@
-var express = re
+var express = require ('express');
+var app = express();
+var server = require ('http').Server(app);
+var io = require ('socket.io')(server);
+var fs = require("fs")
+
+app.use(express.static("."))
